93ee23eacdf351200f6d53e3f4a63c26d9a6fecb
Raj Kumar Boddu <rajkumar.boddu1311@gmail.com>
2018-02-01 23:11:19 +0530

    completed splitting image

--- splilt.php
+++ splilt.php
@@ -6,19 +6,21 @@ $size = getimagesize('sample.jpg');
 $width = $size[0];
 $height = $size[1];
 $ext = image_type_to_extension($size[2], false);
-/*echo $ext;
-echo "<pre>";
-var_dump($size);
-echo "</pre>";*/
 $tile_width = intval($width/$cols);
 $tile_height = intval($height/$rows);
-$dest = imagecreatetruecolor($tile_width, $tile_height);
+
 if($ext === "png"){
 
 } elseif($ext === "jpg" || $ext === "jpeg"){
 	$src = imagecreatefromjpeg($file_with_path);
-	imagecopy($dest, $src, 0, 0, 0, 0, $tile_width, $tile_height);
-	$result = imagejpeg($dest, "tile1.jpeg");
-	var_dump($result);
+	for($i=0; $i<$rows; $i++){
+		for($j=0; $j<$cols; $j++){
+			$dest = imagecreatetruecolor($tile_width, $tile_height);
+			imagecopy($dest, $src, 0, 0, $j*$tile_width, $i*$tile_height, $tile_width, $tile_height);
+			$result = imagejpeg($dest, "tile$i$j.jpeg");
+			echo "<img src='tile$i$j.jpeg' style='border:1px solid black;' />";
+		}
+		echo "<br>";
+	}
 }
 ?>
